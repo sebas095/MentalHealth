ec117e9cb9ab9c39b8ded58b80fa80cd53e901b4
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2016-03-10 17:27:24 +0000

    Some fixed

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Project MentalHealt' });
+  res.render('index', { title: 'Project MentalHealth' });
 });
 
 module.exports = router;
