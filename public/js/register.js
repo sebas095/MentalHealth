e0dd6d76a9dc0f22a2ec65d7584c9a85395ff017
Sebastian Duque <sebas_tian_95@hotmail.com>
2016-03-23 15:07:05 -0500

    add some efects for users register

---
+++ public/js/register.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('#users').click(function() {
+    $('#userForm').show();
+    $('#epsForm').hide();
+    $('#submitForm').show();
+  });
+
+  $('#eps').click(function() {
+    $('#userForm').hide();
+    $('#epsForm').show();
+    $('#submitForm').show();
+  });
+});
