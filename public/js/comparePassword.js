e6a27d075c179cc35642b4580f60561401c536ac
Sebastian Duque <sebas_tian_95@hotmail.com>
2016-03-24 23:21:02 -0500

    add more validations

--- public/js/comparePassword.js
+++ public/js/comparePassword.js
@@ -1,6 +1,6 @@
 function compare(form) {
   if (form.pwd.value != form.cpwd.value) {
-    alert("La contraseñas no coinciden");
+    alert("Las contraseñas no coinciden");
     return false;
   }
   alert("Pronto le enviaremos un correo con el estado de aprobación de su cuenta");
