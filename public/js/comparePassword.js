1c1d1f1fa95b027ce9184c620e81c78db3c43c7a
Sebastian Duque <sebas_tian_95@hotmail.com>
2016-03-25 17:18:46 -0500

    add root alert

--- public/js/comparePassword.js
+++ public/js/comparePassword.js
@@ -1,8 +1,9 @@
-function compare(form) {
+function compare(form, rol) {
   if (form.pwd.value != form.cpwd.value) {
     alert("Las contraseñas no coinciden");
     return false;
   }
-  alert("Pronto le enviaremos un correo con el estado de aprobación de su cuenta");
+  if (rol) alert("Pronto le enviaremos un correo con el estado de aprobación de su cuenta");
+  else alert("El usuario Root ha sido creado con éxito!");
   return true;
 }
