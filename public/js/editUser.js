f4078ab37d18038af880276159af0afacc3855f9
Sebastian Duque <sebas_tian_95@hotmail.com>
2016-03-27 17:14:55 -0500

    add data for edit account

--- public/js/editUser.js
+++ public/js/editUser.js
@@ -1,10 +1,14 @@
 function comparePwd(form) {
-  if (form.pwd.value || form.pwd.value) {
+  if (form.pwd.value && form.cpwd.value) {
     if (form.pwd.value != form.cpwd.value) {
       alert("Las contraseñas no coinciden");
       return false;
     }
+    return true;
   }
-
+  if (form.pwd.value || form.pwd.value) {
+    alert('Si deseas cambiar de contraseña por favor llena ambos campos');
+    return false;
+  }
   return true;
 }
