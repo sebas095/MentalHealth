bdeca5f5a684be8f7945fc1da9461ab0d979fef8
Sebastian Duque <sebas_tian_95@hotmail.com>
2016-03-28 22:08:23 -0500

    add images default

--- app.js
+++ app.js
@@ -36,11 +36,6 @@ app.use(flash());
 
 // Helpers dinamicos:
 app.use(function(req, res, next){
-  // guardar path en session.redir para despues de login
-  // if(!req.path.match(/\/login|\/logout/)){
-  //  req.session.redir = req.path;
-  // }
-
   // Hacer visible req.session en las vistas
   res.locals.session = req.session;
   res.locals.session.url = urlApp || 'http://localhost:8080/';
