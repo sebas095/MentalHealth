fa82fffbf98e79e1fc120ee067bab6015928426a
Sebastian Duque <sebas_tian_95@hotmail.com>
2016-04-03 19:12:01 -0500

    add menu user

--- public/js/pending.js
+++ public/js/pending.js
@@ -1,6 +1,6 @@
 $(function() {
   var path = (window.location.pathname).split('/');
-  var ok = (path[3] == 'eps' || path[3] == 'root' || path[3] == 'medicoGeneral' || path[3] == 'medicoEspecialista');
+  var ok = (path[3] == 'eps' || path[3] == 'root' || path[3] == 'paciente' || path[3] == 'medicoGeneral' || path[3] == 'medicoEspecialista');
 
   if (path.length == 4 && path[1] == 'users' && ok) {
     $('#pending').show();
