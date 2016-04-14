d320a08418ced47d04df3b1982c7838280bd587e
Sebastian Duque <sebas_tian_95@hotmail.com>
2016-04-13 20:27:01 -0500

    add basic view of calendar for users

--- controllers/calendar.js
+++ controllers/calendar.js
@@ -2,5 +2,14 @@ const User = require('../models/user');
 const uuid = require('uuid');
 
 exports.home = function(req, res) {
+  if (req.params.rol == 'paciente') {
+    res.render('users/calendar/paciente');
+  }
+  else {
+    res.render('users/calendar/medicos');
+  }
+}
+
+exports.pending = function(req, res) {
   res.render('users/calendar/index');
 }
--- routes/users.js
+++ routes/users.js
@@ -10,74 +10,47 @@ const calendarController = require('../controllers/calendar');
 module.exports = function(app, mountPoint) {
   // GET
   router.get('/new', userController.new);
-
   router.get('/:id/:rol(\\eps|paciente|root|medico(General|Especialista))', sessionController.loginRequired, userController.home);
-
   router.get('/:id/edit', sessionController.loginRequired, userController.edit);
-
   router.get('/:id/:rol(\\eps|paciente|root|medico(General|Especialista))/edit', sessionController.loginRequired, userController.rolForm);
-
+  router.get('/:id/paciente/pendingList', sessionController.loginRequired, calendarController.pending);
   router.get('/:id/:rol(\\paciente|medico(General|Especialista))/calendar', sessionController.loginRequired, calendarController.home);
-
   router.get('/:id/:rol(\\eps|root)/pending', sessionController.loginRequired, userController.pending);
-
   router.get('/:id/root/manage', sessionController.loginRequired, rootController.manage);
-
   router.get('/:id/eps/manage', sessionController.loginRequired, epsController.manage);
-
   router.get('/:id/eps/manage/editRol', sessionController.loginRequired, epsController.chooseRol);
-
   router.get('/:id/root/manage/edit', sessionController.loginRequired, rootController.manageProfile);
-
   router.get('/:id/eps/manage/edit', sessionController.loginRequired, epsController.manageProfile);
-
   router.get('/:id/:rol(\\eps|root)/allow', sessionController.loginRequired, userController.allow);
-
   router.get('/:id/data', sessionController.loginRequired, userController.dataRol);
 
   // POST
   router.post('/create/root', rootController.create);
-
   router.post('/create/eps', epsController.create);
-
   router.post('/create/user', userController.create);
-
   router.post('/:id/:rol(\\eps|root)/pending', sessionController.loginRequired, userController.allowUser);
-
   router.post('/:id/:rol(\\paciente|medico(General|Especialista))/calendar', function(req, res) {
     res.render('index');
   });
 
   // PUT
   router.put('/:id/edit', sessionController.loginRequired, userController.saveChanges);
-
   router.put('/:id/:rol(\\eps|paciente|root|medico(General|Especialista))/edit', sessionController.loginRequired, userController.editRol);
-
   router.put('/:id/eps/allow', sessionController.loginRequired, epsController.allowUsers);
-
   router.put('/:id/root/allow', sessionController.loginRequired, rootController.allowEps);
-
   router.put('/:id/:rol(\\paciente|medico(General|Especialista))/calendar', function(req, res) {
     res.render('index');
   });
-
   router.put('/:id/root/manage/edit', sessionController.loginRequired, rootController.storeChanges);
-
   router.put('/:id/eps/manage/edit', sessionController.loginRequired, epsController.storeChanges);
-
   router.put('/:id/root/manage/editRol', sessionController.loginRequired, rootController.editRolProfile);
-
   router.put('/:id/eps/manage/editRol', sessionController.loginRequired, epsController.editRolProfile);
 
   // DELETE
   router.delete('/:id/edit', sessionController.loginRequired, userController.deleteAccount);
-
   router.delete('/:id/:rol(\\eps|paciente|root|medico(General|Especialista))/edit', sessionController.loginRequired, userController.deleteRolImage);
-
   router.delete('/:id/root/manage/editRol', sessionController.loginRequired, rootController.deleteImageProfile);
-
   router.delete('/:id/eps/manage/editRol', sessionController.loginRequired, epsController.deleteImageProfile);
-
   router.delete('/:id/:rol(\\paciente|medico(General|Especialista))/calendar', function(req, res) {
     res.render('index');
   });
