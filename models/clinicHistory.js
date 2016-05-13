41fb5e41b4c4cf77613be61deade04549b24502f
Sebastian Duque <sebas_tian_95@hotmail.com>
2016-05-12 22:00:17 -0500

    Update clinic history model

--- models/clinicHistory.js
+++ models/clinicHistory.js
@@ -4,56 +4,6 @@ var ClinicHistory = resourceful.define('clinicHistory', function() {
   this.string('id');
   this.string('idPatient');
   this.array('registers');
-  /*this.string('job', {default: ""});
-  this.number('historyNumber');
-  this.string('civilStatus');
-  this.string('currAddress');
-  this.string('pastAddress');
-  this.string('educationLevel');
-  this.string('religion');
-  this.string('reasonConsultation');
-  this.string('diseaseHistory');
-
-  // Antecedentes Heredo Familiares
-  this.string('parentsAlive')
-  this.object('parentsDead');
-  this.string('sibAlive');
-  this.object('sibDead');
-  this.string('sonsAlive');
-  this.object('sonsDead')
-  this.object('DBTParents');
-  this.object('HTAParents');
-  this.object('TBCParents');
-  this.object('GemelarParents');
-  this.object('othersParentsHistory');
-
-  // Antecedentes Personales
-  // Hábitos Tóxicos
-  this.string('alcohol');
-  this.string('snuff');
-  this.string('drugs');
-  this.string('infusions');
-
-  // Fisiológicos
-  this.string('feeding');
-  this.string('Dipsia');
-  this.string('Diuresis');
-  this.string('Catarsis');
-  this.string('Somnia');
-  this.object('othersPhysiological');
-
-  // Patológicos
-  this.string('infancy');
-  this.string('adult');
-  this.object('DBT');
-  this.object('HTA');
-  this.object('TBC');
-  this.object('Gemelar');
-  this.object('othersTreatments');
-  this.string('surgical');
-  this.string('traumatology');
-  this.string('allergy');
-  this.object('othersPathological');*/
 
   this.timestamps();
 });
